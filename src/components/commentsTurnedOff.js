2bc0d847f57569b9bc3e32d206be59ce4384d72d
Tingting-Jiang <jtting@126.com>
2023-01-01 21:08:49 -0800

    update comments

--- src/components/commentsTurnedOff.js
+++ src/components/commentsTurnedOff.js
@@ -2,10 +2,11 @@ import React from "react";
 const CommentsTurnedOff = ()=>{
     return (
         <div className="center" id='center'>
-        <div className='fs-2 border border-3 border-secondary d-flex justify-content-center comments-border'>Comments are turned off.
-        <p className="fs-4">
-            Over the years, our comment section has seen a great deal of misinformation about various health issues, uncivil commentaries, and personal attacks. For that reason, we have turned off the comment section to stop the spread of misinformation and promote a more civil, companionate online environment.  
-            Users who have questions or are looking for guidance can send a direct message, and we will respond as soon as possible. 
+        <div className="border border-3 border-secondary d-flex justify-content-center comments-border">
+            <p className="fs-2">Comments are turned off. </p>
+            <p className="fs-6">
+                Over the years, our comment section has seen a great deal of misinformation about various health issues, uncivil commentaries, and personal attacks. For that reason, we have turned off the comment section to stop the spread of misinformation and promote a more civil, companionate online environment.  
+                Users who have questions or are looking for guidance can send a direct message, and we will respond as soon as possible. 
             </p>
         </div>
        
