baa7e13c21ec21b2a75b37698241415c52a77669
Tingting-Jiang <jtting@126.com>
2023-01-09 16:16:13 -0800

    Update styles

--- src/components/commentsTurnedOff.js
+++ src/components/commentsTurnedOff.js
@@ -4,7 +4,7 @@ const CommentsTurnedOff = ()=>{
         <div className="center" id='center'>
             <div className="border border-3 border-secondary comments-border">
                 <div className="text-center fs-3 pe-2 ps-2">Comments are turned off. </div>
-                <div className="fs-6 pe-2 ps-2">
+                <div className="fs-6 pe-2 ps-2" align="justify">
                     Over the years, our comment section has seen a great deal of misinformation about various health issues, uncivil commentaries, and personal attacks. For that reason, we have turned off the comment section to stop the spread of misinformation and promote a more civil, companionate online environment.  
                     Users who have questions or are looking for guidance can send a direct message, and we will respond as soon as possible. 
                 </div>
